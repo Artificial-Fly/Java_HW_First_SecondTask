5ee932aa1215d8b8779c984d55b176c22bc8afb4
Lazy Fruit <117809440+Artificial-Fly@users.noreply.github.com>
2025-12-17 09:02:35 +0900

    first commit

---
+++ SecondTask.java
@@ -0,0 +1,10 @@
+
+
+public class SecondTask {
+    public static void main(String[] args) {
+        int income = 13000;
+        int spending = 3400;
+        System.out.println("Итого (руб):");
+        System.out.println(income-spending);
+    }
+}
